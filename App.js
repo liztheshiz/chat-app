351a5aba3672cbd8299db91b29d02db1f9b652c8
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-08-11 17:13:57 -0400

    loaded fonts with expo-fonts

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import * as Font from 'expo-font';
+
 import Start from './components/Start';
 import Chat from './components/Chat';
 
@@ -12,7 +14,24 @@ import Chat from './components/Chat';
 const Stack = createStackNavigator();
 
 export default class App extends Component {
+    async loadFonts() {
+        await Font.loadAsync({
+            'Poppins-Regular': require('./assets/fonts/Poppins/Poppins-Regular.ttf'),
+            'Poppins-Bold': require('./assets/fonts/Poppins/Poppins-Bold.ttf'),
+        });
+        this.setState({ fontsLoaded: true });
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fontsLoaded: false
+        }
+    }
+
     render() {
+        if (!this.state.fontsLoaded) { return null }
+
         return (
             <NavigationContainer>
                 <Stack.Navigator
@@ -31,7 +50,10 @@ export default class App extends Component {
                     />
                 </Stack.Navigator>
             </NavigationContainer>
-
         );
+    }
+
+    componentDidMount() {
+        this.loadFonts();
     }
 }
