3b6521dc7905bed98cc8e7f03e48582cc65481f5
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-08-21 12:17:22 -0400

    removed unused "image" from import

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import firebase from 'firebase';
 
